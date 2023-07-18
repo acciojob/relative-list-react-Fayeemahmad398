94275283670c694050aad56c6ecbec9907ea52a4
Fayeemahmad398 <fayeemahmad0@gmail.com>
2023-07-18 17:23:08 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
+  const arr = ["faheem", "suhail", "sajid", "faizan", "salman"];
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol id="relativeList" key={"relativeList"}>
+        {arr.map((name, index) => {
+          return (
+            <li
+            
+              id={`relativeListItem${index}`}
+              key={`relativeListItem${index}`}
+            >
+              {name}
+            </li>
+          );
+        })}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
