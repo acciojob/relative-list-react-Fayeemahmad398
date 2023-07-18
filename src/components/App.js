23b72fa195f0f94769a8e691aeb085127cf6e056
Fayeemahmad398 <fayeemahmad0@gmail.com>
2023-07-18 17:26:46 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,6 @@ const App = () => {
         {arr.map((name, index) => {
           return (
             <li
-            
               id={`relativeListItem${index}`}
               key={`relativeListItem${index}`}
             >
